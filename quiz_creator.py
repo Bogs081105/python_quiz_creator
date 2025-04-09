5c9e24f3f54d4396713670a4d3397a36d8d9ff53
Bogs081105 <tubogkyrie0811@gmail.com>
2025-04-09 16:36:21 +0800

    Pseudo code created

---
+++ quiz_creator.py
@@ -0,0 +1,9 @@
+# import necessary libraries
+# initialize an empty list to store the quiz data
+# use while loop to continuously ask the user
+# ask the user to input questions
+# ask the user to input the choices of the questions [A, B, C, D]
+# ask the user to enter the correct answer of the question
+# add the questions and answers into the empty list
+# save the inputs into the file
+# ask the user if they want to continue or not
