e1c000ee3a3f4dcc45677337625f0e1f8e210936
Bogs081105 <tubogkyrie0811@gmail.com>
2025-04-09 17:52:21 +0800

    fixed error in code which prevented the code to print colors inside the terminal

--- quiz_creator.py
+++ quiz_creator.py
@@ -14,15 +14,25 @@ while True:
     console.print("[bold yellow] Add a New question[/bold yellow]")
 # ask the user to input questions
     question = Prompt.ask("[bold]Please enter your question[/bold]")
+
 # add exit condition
     if question == "exit":
         console.print("[bold green] Exiting the quiz creator...[/bold green]")
         break
+
 # ask the user to input the choices of the questions [A, B, C, D]
-    answer_a = Prompt.ask("[dark_magenta]A[/dark magenta]")
-    answer_b = Prompt.ask("[dark_magenta]B[/dark magenta]")
-    answer_c = Prompt.ask("[dark_magenta]C[/dark magenta]")
-    answer_d = Prompt.ask("[dark_magenta]D[/dark magenta]")
+    console.print("[dark_magenta]Enter answer for A[/dark_magenta]")
+    answer_a = Prompt.ask("A")
+
+    console.print("[dark_magenta]Enter answer for B[/dark_magenta]")
+    answer_b = Prompt.ask("B")
+    
+    console.print("[dark_magenta]Enter answer for C[/dark_magenta]")
+    answer_c = Prompt.ask("C")
+
+    console.print("[dark_magenta]Enter answer for D[/dark_magenta]")
+    answer_d = Prompt.ask("D")
+
 # ask the user to enter the correct answer of the question
 # add the questions and answers into the empty list
 # save the inputs into the file
