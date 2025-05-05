e7bc563a9820b569a64e86dc864af67afbade543
Bogs081105 <tubogkyrie0811@gmail.com>
2025-05-06 01:36:59 +0800

    psuedo code created

---
+++ python_quiz_reader.py
@@ -0,0 +1,10 @@
+# import the necessary libraries
+# Load the quiz data from the file
+# option for empty empty quiz data/not found quiz data
+# Start quiz loop to allow the user to keep taking the quiz until they choose to exit
+# pick a random question from the quiz data
+# display the question
+# Ask the user to input their answer
+# Check if the answer is correct and display the result
+# Update the score and display it
+# Ask the user if they want to continue or exit the quiz
