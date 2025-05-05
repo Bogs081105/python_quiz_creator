31f1b1473c98852d030906bde8dceab9c3182d8e
Bogs081105 <tubogkyrie0811@gmail.com>
2025-05-06 02:04:52 +0800

    added a starting function to open files

--- python_quiz_reader.py
+++ python_quiz_reader.py
@@ -6,6 +6,10 @@ from rich.prompt import Prompt
 from rich.panel import Panel
 
 # Load the quiz data from the file
+with open("quiz_data.txt", "r", encoding="utf-8") as file:
+    quiz_content = file.read().strip()
+
+
 # option for empty empty quiz data/not found quiz data
 # Start quiz loop to allow the user to keep taking the quiz until they choose to exit
 # pick a random question from the quiz data
